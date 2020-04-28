8b8186e23ee9360c5dc68f4fa658042cf98b968a
TheCoreblank <zanegaffori@gmail.com>
2020-04-28 11:32:10 +0100

    version 0.2.1 commit

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from PyQt5.QtWidgets import QApplication, QTextEdit, QPushButton, QFileDialog
 from PyQt5.QtGui import QIcon
 import keyboard as kb
+from json import loads
+xfile = None
+content = None
+filetopngraw = open('filetopng.json', 'r')
+filetopng = loads(filetopngraw.read())
+
 def xopen():
     global xfile
     global content
     filex = ''
-    filetype = {
-        'py' : 'python.png',
-        'md' : 'markdown.png'
-    }
     xfile = QFileDialog().getOpenFileName()[0]
     content = xfile
     long_filename = xfile.split('.')
     filex = long_filename[1]
     with open(xfile, 'r+') as xfile:
         xfile = xfile.read()
-    icon = QIcon('icons/' + filetype[filex])
+    icon = QIcon('icons/' + filetopng[filex])
     app.setWindowIcon(icon)
 
 def save(xxfile):
