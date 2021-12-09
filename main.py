bffd1bcf48f64017ec21494753ffde57fcdb389c
Comix Inc <56793799+tqoo@users.noreply.github.com>
2021-12-09 14:37:45 +0000

    Added fixed PyItor and make_exe command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#https://github.com/microsoft/vscode-icons/#tree/main/icons/light
 from PyQt5.QtWidgets import QApplication, QTextEdit, QPushButton, QFileDialog, QMenuBar, QShortcut, QVBoxLayout, QWidget, QAction
 from PyQt5.QtGui import QIcon, QKeySequence
 from json import loads
+
 xfile = None
 content = None
 filetopngraw = open('filetopng.json', 'r')
@@ -16,14 +18,21 @@ def xopen():
     long_filename = xfile.split('.')
     filex = long_filename[1]
     with open(xfile, 'r+') as xfile:
-        xfile = xfile.read()
-    icon = QIcon('icons/' + filetopng[filex])
+        try:
+            xfile = xfile.read()
+        except UnicodeDecodeError:
+            print("Welp, that can't be read.")
+            quit()
+    try:
+        icon = QIcon('icons/' + filetopng[filex])
+    except KeyError:
+        icon = QIcon('icons/default.png')
     app.setWindowIcon(icon)
     print('setting text')
     text.setText(xfile)
 
 def save(xxfile):
-    with open(xxfile, 'w') as xfile:
+    with open(xxfile, 'w+') as xfile:
         xfile = xfile.write(text.toPlainText())
 #Initiate Widgets
 app = QApplication([])
